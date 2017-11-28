fd517b67693351ee3c44687b6e14c932e908161b
Massachusetts Environmental Public Health Tracking <32905383+ma-dph-beh@users.noreply.github.com>
2017-11-28 08:52:00 -0500

    MA tracking upload
    files for ma tracking

---
+++ matracking/quick-views/instant-atlas-als/map.js
@@ -0,0 +1,26 @@
+{
+	"template":"Single Map (HTML Edition)",
+	"version":"6.7.2_b1,735 (2014-09-24 1438)",
+	"boundingBox":"19012.0185 768402.6535 345695.33150000003 968859.0965",
+	"layers":[
+	{
+		"type":"base-layer",
+		"id":"_TOWNS_POLYM.shp1",
+		"name":"TOWNS_POLYM.shp",
+		"geometry":"polygon",
+		"url":"_TOWNS_POLYM.shp1.js",
+		"visible":true,
+		"symbolSize":15,
+		"fillColor":"#ffffff",
+		"fillOpacity":0.8,
+		"borderColor":"#cccccc",
+		"borderThickness":1,
+		"showLabels":false,
+		"minLabelExtent":0,
+		"maxLabelExtent":1000000,
+		"iconPath":"",
+		"showDataTips":true,
+		"showInLayerList":true
+	}
+	]
+}
