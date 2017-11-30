ea64a56c754cb27c0fa3f3717cd8a37ca16299b6
Massachusetts Environmental Public Health Tracking <32905383+ma-dph-beh@users.noreply.github.com>
2017-11-30 08:06:31 -0500

    file name change

--- climate/map.js
+++ climate/map.js
@@ -5,10 +5,10 @@
 	"layers":[
 	{
 		"type":"base-layer",
-		"id":"_Towns_wCCV_shp1",
+		"id":"Towns_wCCV_shp1",
 		"name":"Towns_wCCV_shp",
 		"geometry":"polygon",
-		"url":"_Towns_wCCV_shp1.js",
+		"url":"Towns_wCCV_shp1.js",
 		"visible":true,
 		"symbolSize":15,
 		"fillColor":"#ffffff",
